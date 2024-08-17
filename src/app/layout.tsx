d69b78dbc0e63d13df72a12e7fe6561ce7eb18f3
billwang7599 <billwang7599@gmail.com>
2024-08-17 00:46:03 -0400

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,41 @@
+'use client'
+
 import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3b82f6',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+    background: {
+      default: '#000000',
+    },
+  },
+  components: {
+    MuiButton : {
+      defaultProps :{
+        color : "secondary",
+      },
+      styleOverrides: {
+        root: {
+          textTransform: 'none', // Disable uppercase transformation globally
+        },
+      }
+    }
+  }
+});
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
